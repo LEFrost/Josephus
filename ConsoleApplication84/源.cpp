dc28063c9f6520cbe4cb335be539018ad1f66d99
彭建 <pengjian@live.cn>
2015-11-29 11:44:19 +0800

    update

--- ConsoleApplication84/源.cpp
+++ ConsoleApplication84/源.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 using namespace std;
-const int peopleNum = 10;//参加人数
-const int interval = 2;//每轮报数
+const int peopleNum = 100;//参加人数
+const int interval = 3;//每轮报数
 void main()
 {
 	int a[peopleNum];
 	int end = peopleNum, n = 0, i = 0;
 	for (int i = 0; i < peopleNum; i++)
 		a[i] = i + 1;
+	cout << "参加学生的人有：";
 	for (int i = 0; i < peopleNum; i++)
 	{
-		cout << "参加学生的人有：";
 		cout << a[i] << " " ;
 	}
 	cout << endl;
+			cout << "淘汰：";
 	while (1)
 	{
 		if (a[i] > 0)
 			n++;
 		if (n == interval)
 		{
-			cout << "淘汰：" << a[i] << " ";
+			cout << a[i] << " ";
 			a[i] = 0;
 			end--;
 			n = 0;
@@ -32,7 +33,7 @@ void main()
 			break;
 	}
 	cout << endl;
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < peopleNum; i++)
 		if (a[i]>0)
 			cout <<"胜利者是："<<a[i];
 }
